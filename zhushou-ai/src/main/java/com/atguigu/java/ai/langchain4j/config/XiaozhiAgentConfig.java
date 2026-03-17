697d0e3102203e3695a85366efc52e1eb572e81e
Kevin6giao <786734028@qq.com>
2026-03-18 00:46:15 +0800

    项目文件内容

---
+++ zhushou-ai/src/main/java/com/atguigu/java/ai/langchain4j/config/XiaozhiAgentConfig.java
@@ -0,0 +1,113 @@
+package com.atguigu.java.ai.langchain4j.config;
+
+import com.atguigu.java.ai.langchain4j.store.MongoChatMemoryStore;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.ChatMemoryProvider;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class XiaozhiAgentConfig {
+
+    @Value("${langchain4j.open-ai.streaming-chat-model.api-key}")
+    private String apiKey;
+
+    @Value("${langchain4j.open-ai.streaming-chat-model.base-url}")
+    private String baseUrl;
+
+    @Value("${langchain4j.open-ai.streaming-chat-model.model-name}")
+    private String modelName;
+
+    @Autowired
+    private MongoChatMemoryStore mongoChatMemoryStore;
+
+    @Bean
+    public ChatMemoryProvider chatMemoryProviderXiaozhi(){
+
+        return memoryId ->
+            MessageWindowChatMemory.builder()
+                    .id(memoryId)
+                    .maxMessages(20)
+                    .chatMemoryStore(mongoChatMemoryStore)
+                    .build();
+
+    }
+
+    @Bean
+    public StreamingChatLanguageModel qwenStreamingChatModel() {
+        return OpenAiStreamingChatModel.builder()
+                .apiKey(apiKey)
+                .baseUrl(baseUrl)
+                .modelName(modelName)
+                .temperature(0.8)
+                .timeout(Duration.ofSeconds(60))
+                .build();
+    }
+
+    @Bean
+    public OpenAiChatModel qwenChatModel() {
+        return OpenAiChatModel.builder()
+                .apiKey(apiKey)
+                .baseUrl(baseUrl)
+                .modelName(modelName)
+                .temperature(0.8)
+                .timeout(Duration.ofSeconds(60))
+                .build();
+    }
+
+   /* @Bean
+    ContentRetriever contentRetrieverXiaozhi() {
+        //使用FileSystemDocumentLoader读取指定目录下的知识库文档
+        //并使用默认的文档解析器对文档进行解析
+        Document document1 = FileSystemDocumentLoader.loadDocument("E:/knowledge/医院信息.md");
+        Document document2 = FileSystemDocumentLoader.loadDocument("E:/knowledge/科室信息.md");
+        Document document3 = FileSystemDocumentLoader.loadDocument("E:/knowledge/神经内科.md");
+        List<Document> documents = Arrays.asList(document1, document2, document3);
+
+        //使用内存向量存储
+        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        //使用默认的文档分割器
+        EmbeddingStoreIngestor.ingest(documents, embeddingStore);
+
+        //从嵌入存储（EmbeddingStore）里检索和查询内容相关的信息
+        return EmbeddingStoreContentRetriever.from(embeddingStore);
+
+    }*/
+   @Autowired
+   private EmbeddingStore embeddingStore;
+
+    @Autowired
+    private EmbeddingModel embeddingModel;
+
+    @Bean
+    ContentRetriever contentRetrieverXiaozhiPincone() {
+
+        // 创建一个 EmbeddingStoreContentRetriever 对象，用于从嵌入存储中检索内容
+        return EmbeddingStoreContentRetriever
+                .builder()
+                // 设置用于生成嵌入向量的嵌入模型
+                .embeddingModel(embeddingModel)
+                // 指定要使用的嵌入存储
+                .embeddingStore(embeddingStore)
+                // 设置最大检索结果数量，这里表示最多返回 1 条匹配结果
+                .maxResults(1)
+                // 设置最小得分阈值，只有得分大于等于 0.8 的结果才会被返回
+                .minScore(0.8)
+                // 构建最终的 EmbeddingStoreContentRetriever 实例
+                .build();
+    }
+
+}
